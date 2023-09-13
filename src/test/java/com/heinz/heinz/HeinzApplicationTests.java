aaee4063794dca825e97c2440c000a3b0b083d5e
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-13 19:20:54 -0300

    Initial commit

---
+++ src/test/java/com/heinz/heinz/HeinzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heinz.heinz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeinzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
